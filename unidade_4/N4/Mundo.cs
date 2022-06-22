79590b744336989a507c58bbf6108bd79a28487e
Yuri Hartmann <yurihartmann0607@gmail.com>
2022-06-21 21:23:49 -0300

    iniciando o N4

---
+++ unidade_4/N4/Mundo.cs
@@ -0,0 +1,167 @@
+﻿
+#define CG_Gizmo
+// #define CG_Privado
+
+using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Collections.Generic;
+using System.Linq;
+using OpenTK.Input;
+using CG_Biblioteca;
+
+namespace gcgcg
+{
+    class Mundo : GameWindow
+    {
+        private static Mundo instanciaMundo = null;
+
+        private Mundo(int width, int height) : base(width, height) { }
+
+        public static Mundo GetInstance(int width, int height)
+        {
+            if (instanciaMundo == null)
+                instanciaMundo = new Mundo(width, height);
+            return instanciaMundo;
+        }
+
+        private CameraOrtho camera = new CameraOrtho();
+        protected List<Objeto> objetosLista = new List<Objeto>();
+        private ObjetoGeometria objetoSelecionado = null;
+        private char objetoId = '@';
+        private bool bBoxDesenhar = false;
+        int mouseX, mouseY;   //TODO: achar método MouseDown para não ter variável Global
+        private bool mouseMoverPto = false;
+        private Retangulo obj_Retangulo;
+        private Circulo circulo_obj;
+        private int JOGADOR = 3;
+        private int BARREIRA = 1;
+        private int FINAL = 5;
+        int[][] map = new []
+        {
+            new [] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 
+            new [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new [] { 1, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, 
+            new [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, 
+            new [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, 
+            new [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, 
+            new [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, 
+            new [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new [] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new [] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 
+        };
+
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            camera.xmin = -300; camera.xmax = 300; camera.ymin = -300; camera.ymax = 300;
+
+            Console.WriteLine(" --- Ajuda / Teclas: ");
+            Console.WriteLine(" [  H     ] mostra teclas usadas. ");
+
+            objetoId = Utilitario.charProximo(objetoId);
+            objetoSelecionado = null;
+
+            GL.ClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        }
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(camera.xmin, camera.xmax, camera.ymin, camera.ymax, camera.zmin, camera.zmax);
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+#if CG_Gizmo
+            Sru3D();
+#endif
+            for (var i = 0; i < objetosLista.Count; i++)
+                objetosLista[i].Desenhar();
+            if (bBoxDesenhar && (objetoSelecionado != null))
+                objetoSelecionado.BBox.Desenhar();
+            this.SwapBuffers();
+        }
+
+        protected override void OnKeyDown(OpenTK.Input.KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.H)
+                Utilitario.AjudaTeclado();
+            else if (e.Key == Key.M)
+            {
+                foreach (var line in map)
+                {
+                    foreach (var i in line)
+                    {
+                        Console.Write($"{i} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else if (e.Key == Key.Escape)
+                Exit();
+            else if (e.Key == Key.E)
+            {
+                Console.WriteLine("--- Objetos / Pontos: ");
+                for (var i = 0; i < objetosLista.Count; i++)
+                {
+                    Console.WriteLine(objetosLista[i]);
+                }
+            }
+            else if (e.Key == Key.O)
+                bBoxDesenhar = !bBoxDesenhar;
+            else if (e.Key == Key.V)
+                mouseMoverPto = !mouseMoverPto;   //TODO: falta atualizar a BBox do objeto
+            else
+                Console.WriteLine(" __ Tecla não implementada.");
+        }
+
+        //TODO: não está considerando o NDC
+        protected override void OnMouseMove(MouseMoveEventArgs e)
+        {
+            mouseX = e.Position.X; mouseY = 600 - e.Position.Y; // Inverti eixo Y
+            if (mouseMoverPto && (objetoSelecionado != null))
+            {
+                objetoSelecionado.PontosUltimo().X = mouseX;
+                objetoSelecionado.PontosUltimo().Y = mouseY;
+            }
+        }
+
+#if CG_Gizmo
+        private void Sru3D()
+        {
+            GL.LineWidth(1);
+            GL.Begin(PrimitiveType.Lines);
+            // GL.Color3(1.0f,0.0f,0.0f);
+            GL.Color3(Convert.ToByte(255), Convert.ToByte(0), Convert.ToByte(0));
+            GL.Vertex3(0, 0, 0); GL.Vertex3(200, 0, 0);
+            // GL.Color3(0.0f,1.0f,0.0f);
+            GL.Color3(Convert.ToByte(0), Convert.ToByte(255), Convert.ToByte(0));
+            GL.Vertex3(0, 0, 0); GL.Vertex3(0, 200, 0);
+            // GL.Color3(0.0f,0.0f,1.0f);
+            GL.Color3(Convert.ToByte(0), Convert.ToByte(0), Convert.ToByte(255));
+            GL.Vertex3(0, 0, 0); GL.Vertex3(0, 0, 200);
+            GL.End();
+        }
+#endif
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mundo window = Mundo.GetInstance(600, 600);
+            window.Title = "CG_N4";
+            window.Run(1.0 / 60.0);
+        }
+    }
+}
